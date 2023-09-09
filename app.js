cc22b0b95bf0c91a8c0c6fc28c630021afaf8c33
Bhooker88 <139884664+Bhooker88@users.noreply.github.com>
2023-09-09 00:27:11 +0000

    cleaned up a couple of lines

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
+// function fromYenToPound
 const fromYenToPound = function(valueInYen){
     let valueInPound = valueInYen *0.8;
     return valueInPound;
 }
-
+// function fromDollarToYen
 const fromDollarToYen = function(valueInDollar){
     let valueInYen = valueInDollar * 127.9;
     return valueInYen;
 }
 
-// we declare the function with the exact name "fromEuroToDollar"
+// function "fromEuroToDollar"
 const fromEuroToDollar = function(valueInEuro){
-    // convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.2;
-    //return the dollar value
     return valueInDollar;
 }
 
-// this is my function that sums two numbers
+//function for sum of 2 numbers
 const sum = (a,b) => {
     return a + b
 }
 
-// just a console log for ourselves.
+
 console.log(sum(7,3))
 
-// export the function to be used on other files 
-// (similar to the keyword `export` when using webpack)
+
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
 
